da222fd545473cbe4cdfb735f419e4983d7e8967
dev212008 <dev212008@gmail.com>
2024-09-13 11:36:24 +0900

    Feat: 240913 회사 백업

--- Home.py
+++ Home.py
@@ -1,5 +1,46 @@
 import streamlit as st
+from datetime import datetime
 
-st.title("Hello World!")
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="♪♪♪"
+)
 
-st.subheader("Welcome to Streamlit!")
+st.title("FullstackGPT Home")
+
+# today = datetime.today().strftime("%H:%M:%S")
+
+# st.title("최종 수정 시각\t" + today)
+
+# model = st.selectbox(
+#     "Choose your model",
+#     ("GPT-3","GPT-4",)
+# )
+
+# if model == "GPT-3":
+#     st.write("저렴한 모델")
+# else:
+#     st.write("비싼 모델")
+
+# name = st.text_input("당신의 이름을 입력해주세요.")
+# st.write(name)
+
+# value = st.slider("temperature", min_value=0.1, max_value=1.0,)
+# st.write(value)
+
+# with st.sidebar:
+#     st.sidebar.title("sidebar title")
+#     st.sidebar.text_input("xxx")
+
+# st.title("title")
+
+# tab_one, tab_two, tab_three = st.tabs(["A", "B", "C"])
+
+# with tab_one:
+#     st.write("a")
+
+# with tab_two:
+#     st.write("b")
+
+# with tab_three:
+#     st.write("c")
--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 Python 3.10.11
 
+#2.5
 #PowerShell에서 가상환경(venv) 실행
 Get-ExecutionPolicy
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 /env/Scripts/Activate.ps1
-
 pip install -r requirements.txt
-
--- source env/bin/activate (Mac일 경우 venv)
 
 -- venv 실행
 .\env\Scripts\Activate.ps1
-
 -- venv 탈출
 deactivate
+
+#2.6
+-- Jupyter Notebook 사용시 Select kernel 해줄 것.
+
+#6 RAG
+-- RAG 방식
+Stuff, Refine, Map Reduce
+
+#7 Document GPT
+streamlit run Home.py
